1d8fbd7e16826be0d05889193b68801cdec7c558
Md Kowsar Hossain <s2110276152@gmail.com>
2024-09-09 22:23:04 +0600

    Javascript part1 practice question

---
+++ Javascript/app.js
@@ -0,0 +1,38 @@
+//Practice C1-Q1
+
+let age = 23;
+age + 2;
+console.log(age);
+
+//Practice C1-Q2
+
+let math = 90;
+let phy = 80;
+let bangla = 100;
+let avg = (math + phy + bangla) / 3;
+console.log(avg);
+
+//Practice C1-Q3
+
+let num = 5;
+let newNum = num++;
+newNum = ++num;
+console.log(newNum);
+
+//Practice C1-Q3
+
+let name = "Kowsar Hossain";
+console.log(name.length);
+
+let firstName = "kowsar";
+console.log(firstName[0]);
+console.log(firstName[5]);
+
+let fName = "apnacollege";
+let lName = 123;
+console.log(fName + lName);
+
+let empty = "";
+console.log(empty.length);
+let space = " ";
+console.log(space.length);
