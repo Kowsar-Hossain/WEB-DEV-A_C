bf9e68937004f02b4c75285f04c4d830b869c202
Md Kowsar Hossain <s2110276152@gmail.com>
2024-09-16 14:53:09 +0600

    Javascript part4 todo app

--- Javascript/app.js
+++ Javascript/app.js
@@ -1,73 +1,29 @@
-//Javascript part-4
+//Todo App
+let todo = [];
 
-for (let i = 1; i <= 15; i += 2) {
-    console.log(i); //Odd numbers
-}
+let req = prompt("Please enter your request");
 
-for (let i = 2; i <= 15; i += 2) {
-    console.log(i); //even numbers
-}
-
-// let n = prompt("Write your number");
-// n = parseInt(n);
-// for (let i = n; i <= n*10; i += n) {
-//     console.log(i); //multiplicaton table
-// }
-
-//Favorite Movie
-
-// let favMovie = "Avatar";
-// let guess = prompt("Guess our Movie");
-// while (guess != favMovie) {
-//     if (guess == "Quit") {
-//         console.log("You Quit");
-//         break;
-//     }
-//     guess = prompt("Wrong guess. Please try again");
-// }
-
-// if (guess == favMovie) {
-//     console.log("Congrats !!");
-// }
-
-//Nested For Loop
-let fruits = ["mango", "apple", "banana", "litchi", "orange"];
-for (let i = 0; i < fruits.length; i++) {
-    console.log(i, fruits[i]);
-}
-
-//Nested Array
-let heros = [
-    ['ironman', 'spiderman', 'thor'],
-    ['superman', 'wonder woman', 'flash']
-];
-
-for (let i = 0; i < heros.length; i++) {
-    console.log(`Lists #${i}`);
-    for (let j = 0; j < heros[i].length; j++) {
-        console.log(heros[i][j]);
+while (true) {
+    if (req == "quit") {
+        console.log("Quiting App");
+        break;
     }
-}
 
-//For of Loop
-let frts = ["mango", "apple", "banana", "litchi", "orange"];
-
-for (fruit of frts) {
-    console.log(fruit);
-}
-
-for (char of "apnacollege") {
-    console.log(char);
-}
-
-//Nested for-of-Loop
-let nayok = [
-    ['ironman', 'spiderman', 'thor'],
-    ['superman', 'wonder woman', 'flash']
-];
-
-for (list of heros) {
-    for (hero of list) {
-        console.log(hero);
+    if (req == "list") {
+        console.log("--------");
+        for (let i = 0; i < todo.length; i++) {
+            console.log(i, todo[i]);
+        }
+        console.log("--------");
+    } else if (req == "add") {
+        let task = prompt("Please enter your task you want to add");
+        todo.push(task);
+        console.log("task Added");
+    } else if (req == "delete") {
+        let idx = prompt("please enter the task index");
+        todo.splice(idx, 1);
+    } else {
+        console.log("Wrong reuest");
     }
+    req = prompt("Please enter your request");
 }
