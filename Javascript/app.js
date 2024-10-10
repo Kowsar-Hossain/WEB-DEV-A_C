4e981112c7e2b68b2f5e9a18b7d1b5dc8ad40e21
Md Kowsar Hossain <s2110276152@gmail.com>
2024-10-10 23:12:40 +0600

    JS Clss-5

--- Javascript/app.js
+++ Javascript/app.js
@@ -1,51 +1,93 @@
-//Assignment Question
+//Objects Literals
+const student = {
+    name: "Kowsar",
+    age: 23,
+    gender: "Male",
+    marks: 94.4,
+    city: ["Rajshahi", "Sirajgonj"]
+};
+console.log(student);
+console.log(student["city"]);
+student.marks = "Dhaka";
+console.log(student.marks);
+console.log(student.gender);
 
-//Q1
-let arr = [1, 2, 3, 4, 5, 6, 2, 3];
-let num = 2;
-for (let i = 0; i < arr.length; i++) {
-    if (arr[i] == num) {
-        arr.splice(i, 1);
+const obj = {
+    1: "mango",
+    2: "Banana",
+    null: "C",
+    true: "A"
+};
+console.log(obj);
+console.log(obj["1"]);
+
+//Object of Objects
+const classInfo = {
+    Aman: {
+        grade: "A+",
+        city: "Delhi"
+    },
+    shradha: {
+        grade: "A",
+        city: "Pune"
+    },
+    Karan: {
+        grade: "0",
+        city: "Mumbai"
     }
-    console.log(arr[i]);
-}
+};
+console.log(classInfo);
+console.log(classInfo.Karan["grade"]);
 
-//Q2
-let number = 287152;
-let count = 0;
 
-let temp = number;
-while (temp > 0) {
-    count++;
-    temp = Math.floor(temp / 10);
-}
-console.log(count);
+//Array of Objects
 
-//Q3
-let nums = 287152;
-let sum = 0;
-let tem = nums;
-while (tem > 0) {
-    digit = tem % 10;
-    sum += digit;
-    tem = Math.floor(tem / 10);
-}
-console.log(sum);
+const studentInfo = [{
+    name: "aman",
+    grade: "A+",
+    city: "Delhi"
+}, {
+    name: "Shradha",
+    grade: "A",
+    city: "Pune"
+}, {
+    name: "Karan",
+    grade: "0",
+    city: "Mumbai"
+}];
+console.log(studentInfo);
 
-//Q4
-let n = prompt("Enter the value of n: ");
-let fact = 1;
-for (let i = 1; i <= n; i++) {
-    fact *= i;
-}
-console.log(`Factorial of ${n} is ${fact}`);
+//Random Integer
 
-//Q5
-let array = [7, 5, 4, 9, 1, 3, 6];
-let large = 0;
-for (let i = 0; i < array.length; i++) {
-    if (array[i] >= array[i + 1]) {
-        large = array[i];
+let rand = Math.random();
+rand = rand * 10;
+rand = Math.floor(rand);
+rand = rand + 1;
+console.log(rand);
+
+console.log(Math.floor(Math.random() * 10) + 1); //Create arandom integer number
+
+//Practice Question-Q1
+console.log(Math.floor(Math.random() * 100) + 1);
+
+//Practice Question-Q2
+console.log(Math.floor(Math.random() * 5) + 1);
+
+//Guess Game
+const max = prompt("Enter your guessing number");
+const random = Math.floor(Math.random() * max) + 1;
+let guess = prompt("Guess your number");
+while (true) {
+    if (guess == "quit") {
+        console.log("User Quit !!");
+        break;
+    }
+    if (guess == random) {
+        console.log(`You are Right !! Random value is : ${random}`);
+        break;
+    } else if (guess < random) {
+        guess = prompt("Your guess was too small !!! Try another one");
+    } else {
+        guess = prompt("Your guess was too large !!! Try another one");
     }
 }
-console.log(`Large number is : ${large}`);
