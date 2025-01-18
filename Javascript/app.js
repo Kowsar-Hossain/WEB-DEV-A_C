099ec13810c3fa0a6445da82caa104540c0ab5c2
Md Kowsar Hossain <s2110276152@gmail.com>
2025-01-18 23:12:45 +0600

    final

--- Javascript/app.js
+++ Javascript/app.js
@@ -1,72 +1,91 @@
-//this-keyword
-const student = {
-    name: "kowsar",
-    age: "23",
-    math: 95,
-    phy: 86,
-    chem: 93,
-    getAvg() {
-        console.log(this);
-        let avg = (this.math + this.phy + this.chem) / 3;
-        console.log(`${this.name} got average marks =${avg}`);
+//forEach
+let arr = [1, 2, 3, 4, 5];
+
+let fun = function(el) {
+    console.log(el)
+};
+arr.forEach(fun); //By function name
+
+arr.forEach(function(el) {
+    console.log(el) //By function defination
+});
+
+arr.forEach((el => {
+    console.log(el) //By arrow function
+}));
+
+//Using Object
+let obj = [{
+        name: "kowsar",
+        marks: 95
+    },
+    {
+        name: "Manik",
+        marks: 85
+    },
+    {
+        name: "Rabby",
+        marks: 78
     }
-};
+];
 
-function getAvg() {
-    console.log(this); //window object
-}
+obj.forEach((student) => {
+    console.log(student.name)
+});
 
-//try catch
-try {
-    console.log(a);
-} catch (err) {
-    console.log("a is not defined");
-    // console.log(err);
-}
+//map
+let array = [1, 2, 3, 4];
 
-//Arrow function
-const sum = (a, b) => {
-    console.log(a + b);
-};
-sum(2, 3);
+let double = array.map((el) => {
+    console.log(el * 2) //Return a same size array
+});
 
-//Implicit function
-const mul = (x, y) => (x * y);
-mul(4, 5);
+let gpa = obj.map((el) => {
+    console.log(el.marks / 10)
+});
 
-//practice Question
-//Q-1
-const square = (n) => (n * n);
-square(5);
+//Filter
+let nums = [2, 1, 3, 4, 6, 8, 7, 10, 11];
+let ans = nums.filter((el) => {
+    return (el % 2 == 0)
+});
+console.log(ans);
 
-//Q-2
-let id = setInterval(() => {
-    console.log("Hello World")
-}, 2000);
+//Reduce
+let number = [1, 2, 3, 4];
+let result = number.reduce((res, el) => (res + el));
+console.log(result);
 
-setTimeout(() => {
-    clearInterval(id)
-}, 10000);
+//Finding maximum in a array
+let n = [3, 5, 6, 8, 4, 12, 32, 43, 24, 41];
 
-//Assignment Question
-let arr = [1, 3, 5, 6, 7, 8];
-
-const arrayAverage = (arr) => {
-    let sum = 0;
-    for (let number of arr) {
-        sum += number;
-    }
-    return sum / arr.length;
-};
-console.log(arrayAverage(arr));
-
-//Q-2
-const isEven = (n) => {
-    if (n % 2 == 0) {
-        return 'even';
+let max = n.reduce((max, el) => {
+    if (el > max) {
+        return el;
     } else {
-        return 'Not even';
+        return max;
     }
+});
+console.log(max);
+
+//Practice Question
+//Q-1
+let mul = [20, 84, 90, 50];
+let check = mul.every((el) => {
+    return el % 10 == 0
+});
+console.log(check);
+
+//Spread
+let arrays = [3, 4, 5, 3, 2, 5, 7, 9, 8];
+console.log(...arrays);
+console.log(...
+    "kowasrhossain");
+
+//Object Literals
+const data = {
+    email: "kowsar143@gmail.com",
+    password: "abcd",
 };
-let n = 7;
-console.log(isEven(n));
+console.log(data);
+const dataCopy = {...data, id: 123 };
