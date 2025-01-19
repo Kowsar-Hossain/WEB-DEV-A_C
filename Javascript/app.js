bf604f78f357f0c82064aef07df729d11b787078
Md Kowsar Hossain <s2110276152@gmail.com>
2025-01-19 15:21:55 +0600

    JS Class-09

--- Javascript/app.js
+++ Javascript/app.js
@@ -1,91 +1,49 @@
-//forEach
-let arr = [1, 2, 3, 4, 5];
-
-let fun = function(el) {
-    console.log(el)
-};
-arr.forEach(fun); //By function name
-
-arr.forEach(function(el) {
-    console.log(el) //By function defination
-});
-
-arr.forEach((el => {
-    console.log(el) //By arrow function
-}));
-
-//Using Object
-let obj = [{
-        name: "kowsar",
-        marks: 95
-    },
-    {
-        name: "Manik",
-        marks: 85
-    },
-    {
-        name: "Rabby",
-        marks: 78
-    }
-];
-
-obj.forEach((student) => {
-    console.log(student.name)
-});
-
-//map
-let array = [1, 2, 3, 4];
-
-let double = array.map((el) => {
-    console.log(el * 2) //Return a same size array
-});
-
-let gpa = obj.map((el) => {
-    console.log(el.marks / 10)
-});
-
-//Filter
-let nums = [2, 1, 3, 4, 6, 8, 7, 10, 11];
-let ans = nums.filter((el) => {
-    return (el % 2 == 0)
-});
-console.log(ans);
-
-//Reduce
-let number = [1, 2, 3, 4];
-let result = number.reduce((res, el) => (res + el));
-console.log(result);
-
-//Finding maximum in a array
-let n = [3, 5, 6, 8, 4, 12, 32, 43, 24, 41];
-
-let max = n.reduce((max, el) => {
-    if (el > max) {
-        return el;
-    } else {
-        return max;
-    }
-});
-console.log(max);
-
+//Class-09
 //Practice Question
-//Q-1
-let mul = [20, 84, 90, 50];
-let check = mul.every((el) => {
-    return el % 10 == 0
-});
-console.log(check);
 
-//Spread
-let arrays = [3, 4, 5, 3, 2, 5, 7, 9, 8];
-console.log(...arrays);
-console.log(...
-    "kowasrhossain");
+let p = document.createElement("p");
+p.innerText = "Hey I'm red!";
+document.querySelector("body").append(p);
 
-//Object Literals
-const data = {
-    email: "kowsar143@gmail.com",
-    password: "abcd",
-};
-console.log(data);
-const dataCopy = {...data, id: 123 };
+p.classList.add("red");
+
+let h3 = document.createElement("h3");
+h3.innerText = "I'm a blue h3";
+document.querySelector("body").append(h3);
+
+h3.classList.add("blue");
+
+let div = document.createElement("div");
+let h1 = document.createElement("h1");
+let para = document.createElement("p");
+
+h1.innerText = "I'm in a div";
+para.innerText = "ME TOO";
+
+div.append(h1);
+div.append(para);
+
+div.classList.add("box");
+document.querySelector("body").append(div);
+
+
+//Assignment Question-1
+let input = document.createElement("input");
+let button = document.createElement("button");
+
+button.innerText = "Click me";
+document.querySelector("body").append(input);
+document.querySelector("body").append(button);
+
+//Assignment Question-2
+input.setAttribute("placeholder", "username");
+btn.setAttribute("id", "btn");
+
+//Assignment Question-3
+let btn = document.querySelector("#btn");
+btn.classList.add("btnStyle");
+
+//Assignment Question-3
+let h = document.createElement("h1");
+h.innerHTML = "<u>DOM Practice</u>";
+document.querySelector("body").append(h);
