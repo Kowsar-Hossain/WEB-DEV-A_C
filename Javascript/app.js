f545d58ebc4b5fc3a50220d99931a9b81490f0aa
Md Kowsar Hossain <s2110276152@gmail.com>
2025-01-23 23:26:07 +0600

    JS class-13

--- Javascript/app.js
+++ Javascript/app.js
@@ -1,97 +1,73 @@
-//Class - 12
-//Async function
+// let btn = document.querySelector("button");
 
-async function greet() {
-    //throw "Invalid";
-    return "Hello";
-}
+// btn.addEventListener("click", async() => {
+//     let facts = await getFetch();
+//     console.log(facts);;
+//     let p = document.querySelector("#result");
+//     p.innerText = facts;
+// })
 
-greet()
-    .then((result) => {
-        console.log("Hello was print : ", result);
-    })
-    .catch((err) => {
-        console.log("Reject the hello : ", err);
-    })
-
-//await keyword
-
-let h1 = document.querySelector("h1");
-
-function
-colorChange(color, delay) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            h1.style.color = color;
-            console.log(`Color changed to ${color}!`);
-            resolve("Color changed");
-        }, delay);
-    });
-}
-
-async function demo() {
-    await colorChange("red", 1000);
-    await colorChange("yellow", 1000);
-    await colorChange("blue", 1000);
-    colorChange("orange", 1000);
-}
-
-
-//JSON to JS Object Conversion
-let jsonRes = '{"fact":"The most popular pedigreed cat is the Persian cat, followed by the Main Coon cat and the Siamese cat.","length":101}';
-
-let validRes = JSON.parse(jsonRes);
-console.log(validRes.length);
-
-//JS object to JSON
-let student = {
-    name: "kowsar",
-    marks: 54
-};
-console.log(JSON.stringify(student));
-
-//API request
+// //API request for async and await
 // let url = "https://catfact.ninja/fact";
-
-// fetch(url)
-//     .then((res) => {
-//         console.log(res);
-//         return res.json();
-//     })
-//     .then((data) => {
-//         console.log(data);
-//         return fetch(url);
-//     })
-//     .then((res) => {
-//         console.log(res);
-//         return res.json();
-//     })
-//     .then((data) => {
-//         console.log(data.fact);
-//     })
-//     .catch((err) => {
-//         console.log("ERROR : ", err);
-//     })
-
-//API request for asyna and await
-let url = "https://catfact.ninja/fact";
-
-async function getFetch() {
-    try {
-        let res = await axios.get(url);
-        console.log(res.data.fact);
-    } catch (e) {
-        console.log("ERROR : ", e);
-    }
-}
-
 
 // async function getFetch() {
 //     try {
-//         let res = await fetch(url);
-//         let data = await res.json();
-//         console.log(data.fact);
+//         let res = await axios.get(url);
+//         return res.data.fact;
 //     } catch (e) {
 //         console.log("ERROR : ", e);
+//         return "No Fact Found";
 //     }
 // }
+
+//API request for a dog image
+// let bttn = document.querySelector("button");
+// let url = "https://dog.ceo/api/breeds/image/random";
+
+// bttn.addEventListener("click", async() => {
+//     let link = await getImage();
+//     let img = document.querySelector("#result");
+//     img.setAttribute("src", link);
+// });
+
+// async function getImage() {
+//     try {
+//         let res = await axios.get(url);
+//         return res.data.message;
+//     } catch (e) {
+//         console.log("ERROR : ", e);
+//         return "No Image Found";
+//     }
+// }
+
+//API request for updating query string
+let url = "http://universities.hipolabs.com/search?name=";
+let btn = document.querySelector("button");
+
+btn.addEventListener("click", async() => {
+    let country = document.querySelector("input").value;
+    console.log(country);
+    let college = await getCollege(country);
+    show(college);
+});
+
+function show(college) {
+    let list = document.querySelector("#list");
+    list.innerText = "";
+    for (col of college) {
+        console.log(col.name);
+
+        let li = document.createElement("li");
+        li.innerText = col.name;
+        list.appendChild(li);
+    }
+}
+
+async function getCollege(country) {
+    try {
+        let res = await axios.get(url + country);
+        return res.data;
+    } catch (e) {
+        console.log("Error :", e);
+    }
+}
