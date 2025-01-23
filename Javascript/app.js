0168597bf493b642dded9b68d717bfdf4aa034dc
Md Kowsar Hossain <s2110276152@gmail.com>
2025-01-23 08:14:18 +0600

    JS class-12

--- Javascript/app.js
+++ Javascript/app.js
@@ -1,86 +1,97 @@
-//Class-11
+//Class - 12
+//Async function
+
+async function greet() {
+    //throw "Invalid";
+    return "Hello";
+}
+
+greet()
+    .then((result) => {
+        console.log("Hello was print : ", result);
+    })
+    .catch((err) => {
+        console.log("Reject the hello : ", err);
+    })
+
+//await keyword
 
 let h1 = document.querySelector("h1");
 
-// function colorChange(color, delay, nextColorChange) {
-//     setTimeout(() => {
-//         h1.style.color = color;
-//         if (nextColorChange) nextColorChange();
-//     }, delay);
-// }
-
-// colorChange("red", 1000, () => {
-//     colorChange("orange", 1000, () => {
-//         colorChange("blue", 1000);
-//     });
-// });
-
-
-function colorChange(color, delay) {
+function
+colorChange(color, delay) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             h1.style.color = color;
-            resolve("Color Changed");
+            console.log(`Color changed to ${color}!`);
+            resolve("Color changed");
         }, delay);
-    })
-}
-
-colorChange("red", 1000)
-    .then(() => {
-        console.log("red color completed");
-        return colorChange("blue", 2000);
-    })
-    .then(() => {
-        console.log("Blue color Comoleted");
-        return colorChange("yellow", 5000);
-    })
-    .then(() => {
-        console.log("Yellow color Changed");
-    })
-    .catch(() => {
-        console.log("Color not Changed !!");
-    })
-
-
-//Callback Hell
-// function saveToDb(data, success, failure) {
-//     let internetSpeed = Math.floor(Math.random() * 10) + 1;
-//     if (internetSpeed > 4) {
-//         success();
-//     } else {
-//         failure();
-//     }
-// }
-
-// saveToDb("apna College", () => {
-//     console.log("Successfully saved data");
-//     saveToDb("Hello World", () => {
-//         console.log("SuccessFull2");
-//     }, () => {
-//         console.log("Weak Connection2");
-//     })
-// }, () => {
-//     console.log("Failure !! Weak Connection");
-// })
-
-//Promises
-
-function saveToDb(data) {
-    return new Promise((resolve, reject) => {
-        let internetSpeed = Math.floor(Math.random() * 10) + 1;
-        if (internetSpeed > 4) {
-            resolve("Data was saved");
-        } else {
-            reject("Weak Connection !!");
-        }
     });
 }
 
-//using then & catch method
-saveToDb("Apna College")
-    .then(() => {
-        console.log("Data was saved");
-    })
-    .catch(() => {
-        console.log("Data was rejected");
-    })
+async function demo() {
+    await colorChange("red", 1000);
+    await colorChange("yellow", 1000);
+    await colorChange("blue", 1000);
+    colorChange("orange", 1000);
+}
+
+
+//JSON to JS Object Conversion
+let jsonRes = '{"fact":"The most popular pedigreed cat is the Persian cat, followed by the Main Coon cat and the Siamese cat.","length":101}';
+
+let validRes = JSON.parse(jsonRes);
+console.log(validRes.length);
+
+//JS object to JSON
+let student = {
+    name: "kowsar",
+    marks: 54
+};
+console.log(JSON.stringify(student));
+
+//API request
+// let url = "https://catfact.ninja/fact";
+
+// fetch(url)
+//     .then((res) => {
+//         console.log(res);
+//         return res.json();
+//     })
+//     .then((data) => {
+//         console.log(data);
+//         return fetch(url);
+//     })
+//     .then((res) => {
+//         console.log(res);
+//         return res.json();
+//     })
+//     .then((data) => {
+//         console.log(data.fact);
+//     })
+//     .catch((err) => {
+//         console.log("ERROR : ", err);
+//     })
+
+//API request for asyna and await
+let url = "https://catfact.ninja/fact";
+
+async function getFetch() {
+    try {
+        let res = await axios.get(url);
+        console.log(res.data.fact);
+    } catch (e) {
+        console.log("ERROR : ", e);
+    }
+}
+
+
+// async function getFetch() {
+//     try {
+//         let res = await fetch(url);
+//         let data = await res.json();
+//         console.log(data.fact);
+//     } catch (e) {
+//         console.log("ERROR : ", e);
+//     }
+// }
